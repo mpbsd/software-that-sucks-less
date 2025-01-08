6ecfd0444ef8dc808793e0be40201773f89e300b
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-01-08 20:33:12 -0300

    spread windows evenly accross the terminal window

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -63,9 +63,9 @@ static const Layout layouts[] = {
 static char dmenumon[2]       = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *lockcmd[]  = { "slock", NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *tmuxcmd[]  = { "st", "-e", "tmux", NULL };
-static const char *mailcmd[]  = { "st", "-e", "mutt", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *tmuxcmd[]  = { "alacritty", "-e", "tmux", NULL };
+static const char *mailcmd[]  = { "alacritty", "-e", "mutt", NULL };
 static const char *browser[]  = { "firefox", "https://duckduckgo.com", NULL };
 
 static const Key keys[] = {
