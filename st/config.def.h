db3dbb78d605b2b45467502bbba8763ba91861e3
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-03-02 11:49:50 -0300

    adjust st's padding

--- st/config.def.h
+++ st/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Lilex Nerd Font Mono:size=14:antialias=true:autohint=true";
-static int borderpx = 7;
+static int borderpx = 6;
 
 /*
  * What program is execed by st depends of these precedence rules:
