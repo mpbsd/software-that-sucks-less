1bdf31ec3258dace7ab2addfc0eb521ccf16fd61
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-03-05 17:34:50 -0300

    minor change

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,19 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 12;       /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminess Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "Terminess Nerd Font Mono:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#2f4f53";
-static const char *colors[][3]      = {
+static const unsigned int borderpx = 2;        /* border pixel of windows */
+static const unsigned int gappx    = 12;       /* gaps between windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+static const int showbar           = 1;        /* 0 means no bar */
+static const int topbar            = 1;        /* 0 means bottom bar */
+static const char *fonts[]         = { "Terminess Nerd Font Mono:size=12" };
+static const char dmenufont[]      = "Terminess Nerd Font Mono:size=12";
+static const char col_gray1[]      = "#222222";
+static const char col_gray2[]      = "#444444";
+static const char col_gray3[]      = "#bbbbbb";
+static const char col_gray4[]      = "#eeeeee";
+static const char col_cyan[]       = "#2f4f53";
+static const char *colors[][3]     = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
   [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
