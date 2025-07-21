cbc2a4b57ef74f46640673dce9d262aec6aa007a
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-07-21 13:08:36 -0300

    change default fonts

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *browser[] = { "firefox", "https://duckduckgo.com", NULL };
+static const char *browser[] = { "chromium", "https://duckduckgo.com", NULL };
 static const char *lockcmd[] = { "slock", NULL };
 static const char *mailcmd[] = { "st", "-e", "mutt", NULL };
 static const char *readcmd[] = { "calibre", NULL };
