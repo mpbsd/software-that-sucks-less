e9cfddb2b993d6d02f8ac9cf9f17593d9eee50df
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2026-01-08 01:50:24 +0000

    surf is the default browser

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *browser[] = { "chromium", "https://duckduckgo.com", NULL };
+static const char *browser[] = { "surf", "https://duckduckgo.com", NULL };
 static const char *lockcmd[] = { "slock", NULL };
 static const char *mailcmd[] = { "st", "-e", "mutt", NULL };
 static const char *strmcmd[] = { "st", NULL };
